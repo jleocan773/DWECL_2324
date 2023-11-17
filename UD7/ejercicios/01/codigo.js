2310cc4f480b0f17433522fde95c71f603fa1e00
Jonathan <jleocan773@g.educaand.es>
2023-11-17 13:10:55 +0100

    JLC | 11/17/2023 | Unidad 7 Empezada

--- UD7/ejercicios/01/codigo.js
+++ UD7/ejercicios/01/codigo.js
@@ -5,9 +5,12 @@ class Vehiculo {
 	}
 
 	validaMatricula() {
+		// Obtengo el campo de matricula
 		var matriculaInput = document.getElementById("form_matricula");
 
+		// Pongo un patrón en mi HTML, hago un oyente para el input del campo.
 		matriculaInput.addEventListener("input", function () {
+			//Si el patrón no concuerda muestro un mensaje customizado
 			if (matriculaInput.validity.patternMismatch) {
 				matriculaInput.setCustomValidity(
 					"Ingrese una matrícula válida (4 números y 3 letras)"
@@ -19,29 +22,23 @@ class Vehiculo {
 	}
 
 	cambiarTituloDNI() {
+		// Obtengo los dos campos del DNI, el título y el campo
 		var tituloDNI = document.getElementById("label_dni");
 		var formDNI = document.getElementById("form_dni");
 
+		// Cuándo el campo está activo (se le hace clic), cambio el título
 		formDNI.addEventListener("focus", function () {
 			tituloDNI.textContent = "Editando DNI";
 		});
 
+		// Lo devuelvo a la normalidad cuándo se hace clic fuera del campo
 		formDNI.addEventListener("blur", function () {
 			tituloDNI.textContent = "DNI";
 		});
 	}
+
 }
 
+
+
 var vehiculo = new Vehiculo();
-
-document
-	.getElementById("form_matricula")
-	.addEventListener("input", vehiculo.validaMatricula);
-
-document.getElementById("form_dni").addEventListener("focus", function () {
-	vehiculo.cambiarTituloDNI(true);
-});
-
-document.getElementById("form_dni").addEventListener("blur", function () {
-	vehiculo.cambiarTituloDNI(false);
-});
---
+++ UD7/ejercicios/02/codigo.js
@@ -0,0 +1,26 @@
+class Login {
+
+
+    #usuarios = {
+        'user': {'login':'adri1', 'password':'chocoflake'},
+        'user': {'login':'jorge2', 'password':'piccolo'},
+        'user': {'login':'jonathan3', 'password':'leon'},
+    }
+
+
+
+    validarFormatoPassword(){
+        var passwordLogin = document.getElementById("password");
+        var patron = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z]).{8,}$/;
+
+        
+
+    }
+
+
+
+
+
+
+
+}
